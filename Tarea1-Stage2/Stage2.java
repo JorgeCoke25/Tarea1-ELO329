88a4405516a84aea44e5b7e8c1db8fa3e74fdcb8
JorgeCoke25 <jorg.barriosn@usm.cl>
2022-04-16 18:33:20 -0400

    Main corregido para "Stage2"

--- Tarea1-Stage2/Stage2.java
+++ Tarea1-Stage2/Stage2.java
@@ -12,22 +12,23 @@ public class Stage2 {
         //System.out.println("File: " + args[0]);
         Cloud cloud = new Cloud();
         // reading <#_de_cortinas> <#_de_lámparas> <#_controles_cortinas> <#_controles_lámparas>
+        int numRollerShades = in.nextInt();
         in.nextInt();  // skip number of roller shades
-        int numLamps = in.nextInt();
-        in.nextInt();  // skip number of roller shade controls
-        int numLampsControl = in.nextInt();
-        // skipping <alfa0> <length0> <canal0> … <alfaN_1> <lengthN_1> <canalN_1>
-        in.nextLine();
-        // creating lamps according to <canal0>…..<canalL_1> for just one lamp
+        int numShadeControls = in.nextInt();
+        in.nextInt(); // skip number of lamp's controls
+        // read <alfa0> <length0> <canal0> … <alfaN_1> <lengthN_1> <canalN_1>
+        double alpha = in.nextDouble();
+        double maxLength = in.nextDouble();
         int channel = in.nextInt();
-        Lamp lamp = new Lamp(channel);
-        cloud.addLamp(lamp);
-        // skipping creation of roller shade's controls at <canal0>
-        in.nextLine();
-        // creating just one lamp's control at <canal0>
+        RollerShade rollerShade = new RollerShade(channel, alpha, maxLength);
+        cloud.addRollerShade(rollerShade);
+        System.out.println("next int:"+ in.nextInt()); // skip lamp's channels
+        // creating just one roller shade's control at <canal0>
         channel = in.nextInt();
-        LampControl lampControl = new LampControl(channel, cloud);
-        Operator operator = new Operator(lampControl, cloud);
+        ShadeControl shadeControl = new ShadeControl(channel, cloud);
+        in.nextInt(); // skipping creation of lamp's control at <canal0>
+        Operator operator = new Operator(shadeControl, cloud);
         operator.executeCommands(in, System.out);
     }
 }
+
