11b9c29fa2a57396b0f033f41485b432def628cd
JorgeCoke25 <jorg.barriosn@usm.cl>
2022-04-16 18:22:24 -0400

    Constructor "DomoticDeviceControl"

--- Tarea1-Stage2/DomoticDeviceControl.java
+++ Tarea1-Stage2/DomoticDeviceControl.java
@@ -1,6 +1,7 @@
 public class DomoticDeviceControl {
     public DomoticDeviceControl(int ch, Cloud c){
-        // ???
+        this.channel=ch;
+        this.cloud=c;
     }
     public int getChannel() {
         return channel;
