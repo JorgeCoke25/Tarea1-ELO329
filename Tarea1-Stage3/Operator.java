9bfdb57d3fed32f215b0a2700dc910b7ab7a877d
JorgeCoke25 <jorge.barriosn@usm.cl>
2022-04-20 20:01:51 -0400

    Correccion Operator
    Se arreglo para el caso donde hay multiples controles.

--- Tarea1-Stage3/Operator.java
+++ Tarea1-Stage3/Operator.java
@@ -1,15 +1,18 @@
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Operator {
     public Operator(Cloud c){
         this.cloud=c;
+        rsControl=new ArrayList<ShadeControl>();
+        lControl=new ArrayList<LampControl>();
     }
     public void addShadeControl(ShadeControl sc){
-        this.rsControl=sc;
+        rsControl.add(sc);
     }
     public void addLampControl(LampControl lc){
-        this.lControl=lc;
+        lControl.add(lc);
     }
     public void executeCommands(Scanner in, PrintStream out){
         out.println("Time\t" + cloud.getHeaders());
@@ -20,79 +23,83 @@ public class Operator {
                 case "C":
                     int channel = in.nextInt();
                     String command=in.next();
-                    if (channel == rsControl.getChannel()) {
-                        switch (command.charAt(0)) {
-                            case 'D': 
-                                rsControl.startDown(); 
-                                break;
-                            case 'U': 
-                                rsControl.startUp();
-                                break;
-                            case 'S': 
-                                rsControl.stop();
-                                break;
-                            default: out.println("Unexpected command:" + command);
-                                System.exit(-1);
+                    for (ShadeControl  rs: rsControl) {
+                        if (channel == rs.getChannel()) {
+                            switch (command.charAt(0)) {
+                                case 'D': 
+                                    rs.startDown(); 
+                                    break;
+                                case 'U': 
+                                    rs.startUp();
+                                    break;
+                                case 'S': 
+                                    rs.stop();
+                                    break;
+                                default: out.println("Unexpected command:" + command);
+                                    System.exit(-1);
+                            }
                         }
                     }
                     break;
                 case "L":
                     int ch = in.nextInt();
                     String com=in.next();
-                    if (ch == lControl.getChannel()) {
-                        switch (com.charAt(0)) {
-                            case 'P':
-                                cloud.changeLampPowerState(ch);
-                                break;
-                            case 'R':
-                                String R=in.next();
-                                switch (R.charAt(0)) {
-                                    case 'U':
-                                        cloud.turnRedUp(ch);
-                                        break;
-                                    case 'D':
-                                        cloud.turnRedDown(ch);
-                                        break;
-                                    default:
-                                        out.println("Unexpected command:" + R);
-                                        System.exit(-1);
-                                        break;
-                                }
-                                break;
-                            case 'G':
-                                String G=in.next();
-                                switch (G.charAt(0)) {
-                                    case 'U':
-                                        cloud.turnGreenUp(ch);
-                                        break;
-                                    case 'D':
-                                        cloud.turnGreenDown(ch);
-                                        break;
-                                    default:
-                                        out.println("Unexpected command:" + G);
-                                        System.exit(-1);
-                                        break;
-                                }
-                                break;
-                            case 'B':
-                                String B=in.next();
-                                switch (B.charAt(0)) {
-                                    case 'U':
-                                        cloud.turnBlueUp(ch);
-                                        break;
-                                    case 'D':
-                                        cloud.turnBlueDown(ch);
-                                        break;
-                                    default:
-                                        out.println("Unexpected command:" + B);
-                                        System.exit(-1);
-                                        break;
-                                }
-                                break;
-                            default:
-                                out.println("Unexpected command:" + com);
-                                System.exit(-1);
-                                break;
+                    for (LampControl lampControl : lControl) {
+                        if (ch == lampControl.getChannel()) {
+                            switch (com.charAt(0)) {
+                                case 'P':
+                                    cloud.changeLampPowerState(ch);
+                                    break;
+                                case 'R':
+                                    String R=in.next();
+                                    switch (R.charAt(0)) {
+                                        case 'U':
+                                            cloud.turnRedUp(ch);
+                                            break;
+                                        case 'D':
+                                            cloud.turnRedDown(ch);
+                                            break;
+                                        default:
+                                            out.println("Unexpected command:" + R);
+                                            System.exit(-1);
+                                            break;
+                                    }
+                                    break;
+                                case 'G':
+                                    String G=in.next();
+                                    switch (G.charAt(0)) {
+                                        case 'U':
+                                            cloud.turnGreenUp(ch);
+                                            break;
+                                        case 'D':
+                                            cloud.turnGreenDown(ch);
+                                            break;
+                                        default:
+                                            out.println("Unexpected command:" + G);
+                                            System.exit(-1);
+                                            break;
+                                    }
+                                    break;
+                                case 'B':
+                                    String B=in.next();
+                                    switch (B.charAt(0)) {
+                                        case 'U':
+                                            cloud.turnBlueUp(ch);
+                                            break;
+                                        case 'D':
+                                            cloud.turnBlueDown(ch);
+                                            break;
+                                        default:
+                                            out.println("Unexpected command:" + B);
+                                            System.exit(-1);
+                                            break;
+                                    }
+                                    break;
+                                default:
+                                    out.println("Unexpected command:" + com);
+                                    System.exit(-1);
+                                    break;
+                            }
                         }
                     }
                     break;
@@ -112,8 +119,8 @@ public class Operator {
         }
     }
     private double time=0;
-    private ShadeControl rsControl;
-    private LampControl lControl;
+    private ArrayList<ShadeControl> rsControl;
+    private ArrayList<LampControl> lControl;
     private Cloud cloud;
     private final double delta=0.1;
 }
